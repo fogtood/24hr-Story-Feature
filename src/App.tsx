2f0e233363abdcba892df02f67e33dde759bd90e
FJ-AKANDE <akandefortunatus2021@gmail.com>
2025-01-07 02:16:28 +0100

    Update index.html title and implement Container component for 24hr Story feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Container from "./components/container.component";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <p className="text-2xl font-bold">24hr Story Feature</p>
+      <Container />
     </>
   );
 };
---
+++ src/components/container.component.tsx
@@ -0,0 +1,9 @@
+const Container = () => {
+  return (
+    <div className="mx-auto min-h-screen max-w-3xl bg-gray-50 p-5">
+      <h1 className="text-2xl font-bold text-black">Stories</h1>
+    </div>
+  );
+};
+
+export default Container;
